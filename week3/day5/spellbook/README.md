af4cca40e701b8b5541ed23767ac51c3fce7e6e0
Jsatchell03 <jsatchell2017@gmail.com>
2026-02-16 23:49:31 -0500

    Spellbook console project

---
+++ week3/day5/spellbook/README.md
@@ -0,0 +1,15 @@
+#Spellbook Manager
+
+This is a simple JavaScript spellbook system that stores and manages spells using objects and arrays.
+
+### How to Run
+
+1. Save the file as spellbook.js.
+2. Open a terminal in the project folder.
+3. Run the program using: `node spellbook.js`.
+
+### Features Implemented
+- Add new spells dynamically with addSpell()
+- Print formatted spell details using object destructuring
+- Generate spell statistics (total spells, average power, highest power spell)
+- Compare spells by power and mana efficiency
---
+++ week3/day5/spellbook/spellbook.js
@@ -0,0 +1,52 @@
+let spellbook = [
+  { name: "fireball", element: "fire", power: 15, manaCost: 5 },
+  { name: "iceShard", element: "ice", power: 12, manaCost: 4 },
+  { name: "thunderStrike", element: "lightning", power: 18, manaCost: 6 },
+  { name: "stoneFist", element: "earth", power: 14, manaCost: 5 },
+  { name: "windSlash", element: "air", power: 10, manaCost: 3 },
+  { name: "shadowBolt", element: "dark", power: 16, manaCost: 6 },
+  { name: "holyLight", element: "light", power: 13, manaCost: 4 },
+  { name: "poisonMist", element: "poison", power: 11, manaCost: 4 },
+  { name: "arcaneBlast", element: "arcane", power: 20, manaCost: 8 },
+  { name: "waterWhip", element: "water", power: 12, manaCost: 4 },
+];
+
+const addSpell = (name, element, power, manaCost) => {
+  const newSpell = {
+    name: name,
+    element: element,
+    power: power,
+    manaCost: manaCost,
+  };
+  spellbook.push(newSpell);
+};
+
+const printSpell = ({ name, element, power, manaCost }) => {
+  console.log(
+    `${name}\nElement: ${element}\nPower: ${power}\nMana Cost: ${manaCost}`,
+  );
+};
+
+const spellSummary = () => {
+  const totalSpells = spellbook.length;
+  const highestPowerSpell = spellbook.reduce((spell, currSpell) => {
+    if (currSpell.power > spell.power) {
+      spell = currSpell;
+    }
+    return spell;
+  }, spellbook[0]).name;
+  const averagePower =
+    spellbook.reduce((acc, curr) => (acc += curr.power), 0) / totalSpells;
+  return `Total Spells: ${totalSpells}\nAverage Power: ${averagePower}\nHighest power spell name: ${highestPowerSpell}`;
+};
+
+const compareSpells = (spellA, spellB) => {
+  const { name: nameA, power: powerA, manaCost: manaA } = spellA;
+  const { name: nameB, power: powerB, manaCost: manaB } = spellB;
+
+  const strongerSpell = powerA > powerB ? nameA : nameB;
+  const moreEfficientSpell = powerA / manaA > powerB / manaB ? nameA : nameB;
+
+  console.log(`Stronger Spell: ${strongerSpell}`);
+  console.log(`More Mana Efficient: ${moreEfficientSpell}`);
+};
